80e21e8ca807b29761dbf29d1ada7e180c03df69
Kamil Pachuta <pachutakamil@gmail.com>
2023-03-31 00:03:43 +0200

    Commit after migration to .Net6 from .Net5

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,36 +1,128 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using NLog.Web;
+using RestaurantAPI.Authorization;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Middleware;
+using RestaurantAPI.Models.Validators;
+using RestaurantAPI.Models;
+using RestaurantAPI;
+using System.Text;
+using FluentValidation.AspNetCore;
+using RestaurantAPI.Services;
+using System.Reflection;
 
-namespace RestaurantAPI
+var builder = WebApplication.CreateBuilder(args);
+
+// NLog: Setup NLog for Dependency injection
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
+//// Add builder.Services to the container.
+
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddSingleton(authenticationSettings);
+builder.Services.AddAuthentication(option =>
 {
-    public class Program
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey))
+    };
+});
+builder.Services.AddAuthorization(option =>
+{
+    option.AddPolicy("HasNationality", builder => builder.RequireClaim("Nationality", "German", "Polish")); // w requireClaim pierwsza wartoœæ to nazwa claimu, kolejne to wartosci które musza byc zgodne
+    option.AddPolicy("Atleast18", builder => builder.AddRequirements(new MinimumAgeRequirement(18)));
+    option.AddPolicy("Atleast2Restaurants", builder => builder.AddRequirements(new CreatedRestaurantsRequirement(2)));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            })
-            .UseNLog();
-    }
-}
+builder.Services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
+builder.Services.AddScoped<IAuthorizationHandler, ResourceOperationRequirementHandler>();
+builder.Services.AddScoped<IAuthorizationHandler, CreatedRestaurantsRequirementHandler>();
+builder.Services.AddControllers().AddFluentValidation();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<IDishService, DishService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+builder.Services.AddScoped<IValidator<LoginDto>, LoginDtoValidator>();
+builder.Services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValidator>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddScoped<RequestTimeMiddleware>();
+builder.Services.AddScoped<IUserContextService, UserContextService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontEndClient", policyBuilder =>
+    {
+        policyBuilder
+        .AllowAnyHeader()
+        .WithOrigins(builder.Configuration["AllowedOrigins"]);
+        //.AllowAnyOrigin(); //Zezwala na dostêp ka¿dej witrynie
+        //.WithOrigins("http://localhost:...") // Dostêp dla konkretnej domeny - nazwe domeny przeniesiono do appsettings dlatego ¿e dla konkretnego urzadzenia bêziemy chcieli hostowaæ ró¿ne frontEndy
+    });
+});
 
-
-//Kod najwy¿szego poziomu automatycznie wygenerowany
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-
-//var app = builder.Build();
+var app = builder.Build();
 
 //// Configure the HTTP request pipeline.
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+
+app.UseResponseCaching();
+app.UseStaticFiles(); //Aplikacaja jest w stanie serowaæ pliki z domyœlnego folderu wwwroot który musi siê znaleŸæ w g³ównym folderze aplikacji
+app.UseCors("FrontEndClient");
+seeder.Seed();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<RequestTimeMiddleware>();
+app.UseAuthentication();
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantAPI");
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
+
+
+
 
 //app.UseHttpsRedirection();
 
@@ -38,4 +130,4 @@ namespace RestaurantAPI
 
 //app.MapControllers();
 
-//app.Run();
+
